b5c0c6b5d7c61033b1da748ea6af85ad0c824cca
Andika Dwiki <andika0106dwiki@gmail.com>
2023-03-18 15:57:42 +0700

    push

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -64,11 +64,7 @@
                                     <div class="form-group">
                                         <div class="d-block">
                                             <label for="password" class="control-label">Password</label>
-                                            <div class="float-right">
-                                                <a href="auth-forgot-password.html" class="text-small">
-                                                    Forgot Password?
-                                                </a>
-                                            </div>
+
                                         </div>
                                         <input id="password" type="password"
                                             class="form-control @error('password') is-invalid @enderror" name="password"
@@ -92,7 +88,9 @@
                             </div>
                         </div>
                         <div class="mt-5 text-muted text-center">
-                            Don't have an account? <a href="auth-register.html">Create One</a>
+                            Don't have an account? <a href="{{ route('register') }}">Create One</a>
+                            <p>Email : admin@gmail.com</p>
+                            <p>Password : password</p>
                         </div>
 
                     </div>
